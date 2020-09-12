6a18593ee8152f4f84e5c4fcdcab8ce8cca811ed
jenny ma <jenny_lau_8@hotmail.com>
2020-09-11 22:12:49 -0400

    E7

--- src/components/ArtistRoute/ArtistRoute.js
+++ src/components/ArtistRoute/ArtistRoute.js
@@ -1,9 +1,35 @@
 import React from "react";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
+import { useParams } from "react-router-dom";
+import { fetchArtistProfile } from "../../helpers/api-helpers";
+import {
+  requestArtistProfile,
+  receiveArtistProfile,
+  requestArtistProfileError,
+} from "../../actions";
 
 const ArtistRoute = () => {
   const accessToken = useSelector((state) => state.auth.token);
-  console.log(accessToken);
+  const currentArtist = useSelector((state) => state.artists);
+  const { id } = useParams();
+
+  const dispatch = useDispatch();
+
+  React.useEffect(() => {
+    if (!accessToken) {
+      return;
+    }
+    dispatch(requestArtistProfile());
+
+    fetchArtistProfile(accessToken, id).then((json) =>
+      dispatch(receiveArtistProfile(json))
+    );
+  }, [accessToken, id]);
+
+  if (currentArtist.status === "loading") {
+    return <h1>Loading...</h1>;
+  }
+
   return accessToken;
 };
 
